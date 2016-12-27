71270d127e4642b7b095d281745e889e00cade22
Viet <viet.le175@gmail.com>
2016-12-26 23:52:53 -0500

    small setup

--- client/modules/home/home.component.ts
+++ client/modules/home/home.component.ts
@@ -2,12 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "home",
+    styleUrls: [`client/modules/home/home.component.css`],
     templateUrl: `client/modules/home/home.component.html`
 })
 export class HomeComponent {
-
-
     constructor() {}
-
-
 }
