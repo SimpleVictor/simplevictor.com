8d9309bec99ef5f54e2473bd0c989b5090374338
Viet <viet.le175@gmail.com>
2016-12-27 18:05:18 -0500

    transition

--- client/modules/home/home.component.ts
+++ client/modules/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from "@angular/core";
+import {Component, AfterViewInit} from "@angular/core";
+
+declare var TweenMax;
+declare var Circ;
+declare var Back;
+declare var $;
 
 @Component({
     selector: "home",
     styleUrls: [`client/modules/home/home.component.css`],
     templateUrl: `client/modules/home/home.component.html`
 })
-export class HomeComponent {
-    constructor() {}
+// TweenMax.from(logo, 2 ,{top: "30%", repeat: 'infinite', yoyo: true, ease: Back.easeInOut});
+export class HomeComponent implements AfterViewInit {
+
+    profileImg;
+    listContainer;
+
+
+    constructor() {
+
+    }
+
+    ngAfterViewInit(){
+        this.profileImg = $(".profile-img")[0];
+        this.listContainer = $(".list-container")[0];
+
+        console.log(this.profileImg);
+        TweenMax.from(this.profileImg, 1 ,{scale: 0, ease: Circ.easeOut});
+        TweenMax.from(this.listContainer, 1 ,{"-webkit-padding-start": "108rem", ease: Back.easeInOut, delay: 1.5});
+
+    }
+    
 }
