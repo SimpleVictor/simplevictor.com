516b798161a2a4bb38110f9fb4385a8357f86778
VietLe <viet.le175@gmail.com>
2016-12-28 02:47:52 -0500

    second page

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,12 +1,50 @@
-import { Component } from "@angular/core";
+import {Component, AfterViewInit} from "@angular/core";
+
+declare var TweenMax;
+declare var Circ;
+declare var Back;
+declare var Bounce;
+declare var $;
 
 @Component({
     selector: "app",
-    template: `<router-outlet></router-outlet>`
+    styleUrls: [`client/css/WelcomePage/full.css`,
+        `client/css/WelcomePage/iphone.css`,
+        `client/css/WelcomePage/med.css`,
+        `client/css/WelcomePage/main.css`],
+    templateUrl: `./client/app.component.html`
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
+    profileImg;
+    listContainer;
     constructor() {
+
+    }
+
+    ngAfterViewInit(){
+        //STARTING POINT - ANIMATE CONTAINERS IN
+        this.profileImg = $(".profile-img")[0];
+        this.listContainer = $(".list-container")[0];
+        TweenMax.from(this.profileImg, 1 ,{scale: 0, ease: Circ.easeOut});
+        TweenMax.from(this.listContainer, 1 ,{"-webkit-padding-start": "100rem", ease: Bounce.easeOut, delay: 0.5});
+
+        //WAIT FOR ON CLICK TO ANIMATE TO THE SECOND PHASE
+    }
+
+    GoToProjects(){
+
+    }
+
+    GoToResume(){
+
+    }
+
+    GoToGithub(){
+
+    }
+
+    GotoAwards(){
 
     }
 
--- client/modules/home/home.component.ts
+++ client/modules/home/home.component.ts
@@ -1,35 +1,20 @@
-import {Component, AfterViewInit} from "@angular/core";
+import {Component} from "@angular/core";
 
-declare var TweenMax;
-declare var Circ;
-declare var Back;
-declare var Bounce;
-declare var $;
+
 
 @Component({
     selector: "home",
-    styleUrls: [`client/modules/home/home.component.css`],
     templateUrl: `client/modules/home/home.component.html`
 })
 // TweenMax.from(logo, 2 ,{top: "30%", repeat: 'infinite', yoyo: true, ease: Back.easeInOut});
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent{
 
-    profileImg;
-    listContainer;
+
 
 
     constructor() {
 
     }
 
-    ngAfterViewInit(){
-        this.profileImg = $(".profile-img")[0];
-        this.listContainer = $(".list-container")[0];
-
-        console.log(this.profileImg);
-        TweenMax.from(this.profileImg, 1 ,{scale: 0, ease: Circ.easeOut});
-        TweenMax.from(this.listContainer, 1 ,{"-webkit-padding-start": "108rem", ease: Bounce.easeInOut});
-
-    }
     
 }
