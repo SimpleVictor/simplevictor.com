de04e6e5e1de324d52dc4cd626db928a8e6e6efc
VietLe <viet.le175@gmail.com>
2016-12-28 05:52:53 -0500

    loader

---
+++ public/assets/extra/preloadImg.js
@@ -0,0 +1,9 @@
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function () {
+        $('<img />').attr('src',this).appendTo('body').css('display','none');
+    });
+}
+
+preload([
+    'assets/img/profile_pic.png'
+]);
