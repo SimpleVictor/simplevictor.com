f68861974bf56599ad28c8e3faf061e2cef2803a
Viet <viet.le175@gmail.com>
2016-12-30 04:07:40 -0500

    pictures has been added / resume pdf

--- client/modules/resume/resume.ts
+++ client/modules/resume/resume.ts
@@ -4,6 +4,10 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "resume",
+    styleUrls: [`client/css/ResumePage/full.css`,
+        `client/css/ResumePage/iphone.css`,
+        `client/css/ResumePage/medium.css`,
+        `client/css/ResumePage/global.css`],
     templateUrl: `client/modules/resume/resume.html`
 })
 
